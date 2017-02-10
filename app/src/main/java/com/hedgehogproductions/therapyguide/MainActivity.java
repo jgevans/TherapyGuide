9f742b39db175c1d5a05657082beaa5aea9ab588
jgevans <jgevans@hedgehogproductions.com>
2017-02-10 13:24:44 +0000

    Adding basic scrolled tabs

--- app/src/main/java/com/hedgehogproductions/therapyguide/MainActivity.java
+++ app/src/main/java/com/hedgehogproductions/therapyguide/MainActivity.java
@@ -1,7 +1,10 @@
 package com.hedgehogproductions.therapyguide;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.support.design.widget.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Create the toolbar
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Create the adapter and add tabs
+
+        TabPagerAdapter mTabPagerAdapter = new TabPagerAdapter(getSupportFragmentManager());
+        mTabPagerAdapter.addFragment(new ListenFragment(), getString(R.string.listen_tab_name));
+        mTabPagerAdapter.addFragment(new DiaryFragment(), getString(R.string.diary_tab_name));
+
+        // Create the pager
+
+        ViewPager mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mTabPagerAdapter);
+
+        // Create tab layout
+
+        TabLayout mTabLayout = (TabLayout) findViewById(R.id.tabs);
+        mTabLayout.setupWithViewPager(mViewPager);
     }
+
 }
---
+++ app/src/main/java/com/hedgehogproductions/therapyguide/TabPagerAdapter.java
@@ -0,0 +1,41 @@
+package com.hedgehogproductions.therapyguide;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+class TabPagerAdapter extends FragmentStatePagerAdapter {
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+    private final List<String> mFragmentTitleList = new ArrayList<>();
+
+    TabPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mFragmentTitleList.get(position);
+    }
+
+    void addFragment(Fragment fragment, String title) {
+        mFragmentList.add(fragment);
+        mFragmentTitleList.add(title);
+    }
+}
+
+
+
