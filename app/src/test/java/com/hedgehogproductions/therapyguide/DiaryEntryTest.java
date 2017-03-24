eb618be5503d36f8ff4f6547ab5c1fa0428f05ae
jgevans <jgevans@hedgehogproductions.com>
2017-03-24 12:32:37 +0000

    Adding first unit test.

---
+++ app/src/test/java/com/hedgehogproductions/therapyguide/DiaryEntryTest.java
@@ -0,0 +1,18 @@
+package com.hedgehogproductions.therapyguide;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+public class DiaryEntryTest {
+
+    @Test
+    public void diaryEntry_CorrectEntry_HasCorrectData() {
+        long timestamp = System.currentTimeMillis();
+        String text = "text1";
+
+        DiaryEntry entry = new DiaryEntry(timestamp, text);
+        assertEquals(entry.getCreationTimestamp(), timestamp);
+        assertEquals(entry.getText(), text);
+    }
+}
